8857465a60f5b9f4e9a5b5012cd5073248334b50
Yevhen Reshetar <reshetar@YevhenMacBook-3063.local>
2022-10-08 15:49:55 +0300

    created profile page, private route and custom hook for protecting the profile page and added the log out functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer} from 'react-toastify';
+
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
@@ -6,8 +8,10 @@ import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Header from "./components/Header";
-import { ToastContainer} from 'react-toastify';
+import PrivateRoute from "./components/PrivateRoute";
+
 import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
@@ -16,7 +20,9 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
